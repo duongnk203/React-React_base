9e9d2ed8bf18b170fd607280cdb81ace115ff57d
Nguyen Kim Duong <duongnkhe171819@fpt.edu.vn>
2025-05-17 20:28:30 +0700

    sua lai base axios theo chuan di lm

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -13,8 +13,9 @@ const TableUser = (props) => {
 
   const getUsers = async () => {
     let res = await fetchAllUser();
-    if (res && res.data && res.data.data) {
-      setListUsers(res.data.data);
+
+    if (res && res.data) {
+      setListUsers(res.data);
     }
   };
 
